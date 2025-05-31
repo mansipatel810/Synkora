637eef49fad165c75f951f3df04c0fc7906669d5
mansipatel <mpmansipatel79@gmail.com>
2025-05-31 16:23:53 +0530

    final1

--- backend/server.js
+++ backend/server.js
@@ -8,11 +8,13 @@ import cookieParser from "cookie-parser"
 import userRoutes from "./routes/user.route.js"
 import chatRoutes from "./routes/chat.route.js"
 import cors from "cors"
-
+import path from "path"
 
 connectDB();
+
+const __dirname=path.resolve(); 
 app.use(cors({
-    origin: process.env.CLIENT_URL || "http://localhost:5173",
+    origin: "http://localhost:5173",
     credentials: true
 }))
 app.use(express.json());
@@ -20,13 +22,21 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
 
-app.get("/", (req,res,next)=>{
-    res.status(200).json({message:"hello from server"})
-})
+// app.get("/", (req,res,next)=>{
+//     res.status(200).json({message:"hello from server"})
+// })
 
 app.use("/api/auth", authRoutes)
 app.use("/api/user", userRoutes)
 app.use("/api/chat", chatRoutes)
+
+if(process.env.NODE_ENV === "production"){
+    app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+    app.get("*", (req,res) =>{
+        res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+    })
+}
 
 const PORT = process.env.PORT || 3001
 app.listen(PORT,()=>{
--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,6 +1,8 @@
 import axios from "axios";
 
+const BASE_URL = import.meta.env.MODE === "development" ? "http://localhost:3001/api" : "/api";
+
 export const axiosInstance = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || "http://localhost:3001/api",
+  baseURL: BASE_URL,
   withCredentials: true,
 }); 
